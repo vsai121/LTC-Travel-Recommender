481e0ba73ae265428900add2316849a384573ed3
V.Saicharan <vsaicharan1998@gmail.com>
2018-04-06 03:31:03 +0530

    Angles working

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from Pathfinder.greedypick import greedyorder
 from Loader.mapLoader import mapLoader
-from Pathfinder.ZoneAngle import validPlaces
+from Pathfinder.ZoneAngle import validPlaces , validate
 import matplotlib.pyplot as plt
 import networkx as nx
 from createMap import makeMap
@@ -17,37 +17,47 @@ labels = nx.get_edge_attributes(loader.Graph,'weight')
 nx.draw_networkx_edge_labels(loader.Graph,pos,edge_labels=labels)
 plt.show()
 
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
 start = input("Enter source:")
+print(start)
 start=loader.places.index(start+'\n')
 tovisit = input("Enter your desired destinations:").split()
 
 for i in range(len(tovisit)):
-    tovisit[i] = loader.places.index(tovisit[i]+"\n")
+	tovisit[i] = loader.places.index(tovisit[i]+"\n")
 allValid = []
 allPaths = []
 
 for i in tovisit:
-    valid = validPlaces(start, i,tovisit,loader)
-    if valid not in allValid:
-        allValid.append(valid)
-        path = greedyorder(start, valid, loader)
-        allPaths.append(path)
+	valid = validPlaces(start, i,tovisit,loader)
+	if valid not in allValid:
+		allValid.append(valid)
+		path = greedyorder(start, valid, loader)
+		allPaths.append(path)
 
 print("allpaths",allPaths)
-for i in allPaths:
+
+for i in powerset(allPaths):
 	s=start
 	finalPath=[]
 	for j in i:
 		for k in nx.shortest_path(loader.Graph,source=s, target=j, weight='weight'):
 			if k not in finalPath:
-
 				finalPath.append(k)
+
+
 		#print("final path",finalPath)
-		
+
 		s=j
 	print("Final path is:",finalPath)
-	validPlaces(start,i[-1],finalPath,loader)
-	#recommend(finalPath)
-	makeMap(finalPath)	
-	print("*"*50)
+	legal = validate(start,finalPath , loader)
+	if legal:
+		makeMap(finalPath)
 
+
+	#recommend(finalPath)
+	print("*"*50)
